b95d4e885c6ad7303def51611e228c5da5fdbfc3
Owen Mak <owdevel@gmail.com>
2021-07-22 19:59:50 +1000

    Added verbose flag to ansible-pull

--- local_run.sh
+++ local_run.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 
 git clone https://github.com/kewlfft/ansible-aur.git ~/.ansible/plugins/modules/aur
-ansible-pull -K -U https://github.com/owdevel/ansible_desktop.git
+ansible-pull -K -v -U https://github.com/owdevel/ansible_desktop.git
 
 
