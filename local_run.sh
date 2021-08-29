eac1c68c43bc30aeb418fe336b0a6e3b79e91036
Owen Mak <owdevel@gmail.com>
2021-08-30 09:41:37 +1000

    Added ansible-galaxy general collection to run script

--- local_run.sh
+++ local_run.sh
@@ -1,6 +1,12 @@
 #/bin/sh
+AUR_PLUGIN_DIR=~/.ansible/plugins/modules/aur/
 
-git clone https://github.com/kewlfft/ansible-aur.git ~/.ansible/plugins/modules/aur
+if [ -d "$AUR_PLUGIN_DIR" ]; then
+	git -C $AUR_PLUGIN_DIR pull
+else
+	git clone https://github.com/kewlfft/ansible-aur.git $AUR_PLUGIN_DIR
+fi
+
+ansible-galaxy collection install community.general
+
 ansible-pull -K -v -U https://github.com/owdevel/ansible_desktop.git
-
-
