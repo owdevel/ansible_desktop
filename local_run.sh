7ed0617f6c6ecad9dc52a4d1bb19de44e0d540ca
Owen Mak <owdevel@gmail.com>
2021-08-30 10:13:06 +1000

    Vs Code Fedora fix and save installer locally

--- local_run.sh
+++ local_run.sh
@@ -1,5 +1,11 @@
 #/bin/sh
 AUR_PLUGIN_DIR=~/.ansible/plugins/modules/aur/
+RUN_SCRIPT=https://raw.githubusercontent.com/owdevel/ansible_desktop/master/local_run.sh
+
+if [ ! -f "$RUN_SCRIPT" ]; then
+	wget https://raw.githubusercontent.com/owdevel/ansible_desktop/master/local_run.sh --output-document $HOME/ansible_desktop.sh
+fi
+
 
 if [ -d "$AUR_PLUGIN_DIR" ]; then
 	git -C $AUR_PLUGIN_DIR pull
