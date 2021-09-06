ae83bf5af4eca678f66f9e34395e2b0d4d71d27a
Owen Mak <owdevel@gmail.com>
2021-09-06 20:06:33 +1000

    Moved aur to galaxy

--- local_run.sh
+++ local_run.sh
@@ -1,5 +1,4 @@
 #/bin/sh
-AUR_PLUGIN_DIR=~/.ansible/plugins/modules/aur/
 RUN_SCRIPT_URL=https://raw.githubusercontent.com/owdevel/ansible_desktop/master/local_run.sh
 RUN_SCRIPT=$HOME/ansible_desktop.sh
 
@@ -15,5 +14,6 @@ else
 fi
 
 ansible-galaxy collection install community.general
+ansible-galaxy collection install kewlfft.aur
 
 ansible-pull -K -v -U https://github.com/owdevel/ansible_desktop.git
