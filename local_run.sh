587e036c4947e3cda710fe898f8e81082636f1eb
Owen Mak <owdevel@gmail.com>
2021-09-06 20:21:12 +1000

    Removed manual clone from local_run.sh

--- local_run.sh
+++ local_run.sh
@@ -6,13 +6,6 @@ if [ ! -f "$RUN_SCRIPT" ]; then
 	wget https://raw.githubusercontent.com/owdevel/ansible_desktop/master/local_run.sh --output-document $RUN_SCRIPT
 fi
 
-
-if [ -d "$AUR_PLUGIN_DIR" ]; then
-	git -C $AUR_PLUGIN_DIR pull
-else
-	git clone https://github.com/kewlfft/ansible-aur.git $AUR_PLUGIN_DIR
-fi
-
 ansible-galaxy collection install community.general
 ansible-galaxy collection install kewlfft.aur
 
